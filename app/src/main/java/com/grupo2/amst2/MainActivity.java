be83dc65f7ae3030299e8360c7a6bbba767f6d17
robertvala <robavala@espol.edu.ec>
2020-12-03 13:41:54 -0500

    paso 4 y 5

--- app/src/main/java/com/grupo2/amst2/MainActivity.java
+++ app/src/main/java/com/grupo2/amst2/MainActivity.java
@@ -2,13 +2,81 @@ package com.grupo2.amst2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View; //Es necesario importer las librerias
+import android.content.Intent;
+
+
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue mQueue;
+    private String token = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void irMenuPrincipal(View v){
+        Intent menuPrincipal = new Intent(getBaseContext(), menu.class);
+        startActivity(menuPrincipal);
+    }
+    private void iniciarSesion(String usuario, String password){
+        Map<String, String> params = new HashMap();
+        params.put("username", usuario);
+        params.put("password", password);
+        JSONObject parametros = new JSONObject(params);
+        String login_url = "https://amst-labx.herokuapp.com/db/nuevo-jwt";
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST, login_url, parametros,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response);
+                        try {
+                            token = response.getString("token");
+                            Intent menuPrincipal = new
+                                    Intent(getBaseContext(), menu.class);
+                            menuPrincipal.putExtra("token", token);
+                            startActivity(menuPrincipal);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                AlertDialog alertDialog = new
+                        AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Alerta");
+                alertDialog.setMessage("Credenciales Incorrectas");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int
+                                    which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
+        mQueue.add(request);
+    }
+
 }
